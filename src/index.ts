acaefaae82e76d60e94e2f3cb7f407874feb2bd8
alexecus <alex.tenepere@gmail.com>
2019-03-10 23:38:19 +0800

    Fix CLI arguments, fix dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
 #!/usr/bin/env node
-
 import program from 'commander';
+
 import { CreateCommand } from './commands/create';
 
 program
     .name('typescipt-starter')
-    .version('1.0');
+    .version('1.0.4');
 
 program
-    .command('new [name]')
+    .command('new <name>')
     .description('Creates a new typescript starter package')
     .action((name: string) => {
         const command = new CreateCommand();
         command.execute(name);
     });
 
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
+
 program.parse(process.argv);
-console.log(process.cwd());
