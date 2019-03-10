368ea5f8a687cbd171a75e73c0251d13368b7124
alexecus <alex.tenepere@gmail.com>
2019-03-10 22:39:14 +0800

    Initial working commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import program from 'commander';
+
+import { CreateCommand } from './commands/create';
+
+program
+    .name('typescipt-starter')
+    .version('1.0');
+
+program
+    .command('new [name]')
+    .description('Creates a new typescript starter package')
+    .action((name: string) => {
+        const command = new CreateCommand();
+        command.execute(name);
+    });
+
+program.parse(process.argv);
+console.log(process.cwd());
